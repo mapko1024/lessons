40be42d512da24cf8503fc6b4a3f7fe7858868a4
mapko1024 <iamarkov1024@gmail.com>
2025-07-14 23:41:09 +0300

    Delete lesson_17_question directory

--- lesson_17_question/js/script.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("click", documentActions);
-
-function documentActions(e) {
-  const targetElement = e.target;
-  if (targetElement.closest(".button-burger")) {
-    document.body.classList.toggle("menu-open");
-  }
-}
